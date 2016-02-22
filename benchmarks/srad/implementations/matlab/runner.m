27eeec0ac106e0cd6a0860f118f62ba734a8fddd
wukefe <wukefe@gmail.com>
2016-02-21 22:55:19 -0500

    Add Matlab implementation for SRAD

---
+++ benchmarks/srad/implementations/matlab/runner.m
@@ -0,0 +1,58 @@
+function runner(niter,lambda)
+% Example: runner(500,1);
+expected_output = 52608;
+
+Nr = 502;
+Nc = 458;
+fileID = fopen('../c/image.pgm','r');
+for i = 1:3
+    fgets(fileID); % skip header
+end
+image  = reshape(fscanf(fileID,'%lf'),Nc,Nr);
+image  = image.';
+fclose(fileID);
+
+% ROI image size
+NeROI = Nr * Nc;
+
+iN = -1 + 1:Nr;
+iS =  1 + 1:Nr;
+jW = -1 + 1:Nc;
+jE =  1 + 1:Nc;
+
+dN = zeros(Nr, Nc);
+dS = zeros(Nr, Nc);
+dW = zeros(Nr, Nc);
+dE = zeros(Nr, Nc);
+
+c  = zeros(Nr, Nc);
+
+% set boundary value
+iN(1)  = 1;
+iS(Nr) = Nr;
+jW(1)  = 1;
+jE(Nc) = 1;
+
+% scale image
+image = exp(image / 255);
+
+tic();
+image = srad_core(image,NeROI,Nc,Nr,iN,iS,jW,jE,dN,dS,dW,dE,c,lambda,niter);
+elapsedTime = toc();
+
+image = log(image) * 255;
+j     = sum(image(1:Nr));
+
+% write_graphics
+if and(niter == 500, lambda == 1)
+    if j ~= expected_output
+        error('expected output of %ld but received %ld instead\n', expected_output, j);
+    end
+else
+    warning('No self-checking step for niter %d and lambda %f\n', niter, lambda);
+end
+
+msg = sprintf('{ \"status\": %d, \"options\": \"%d %f\", \"time\": %f }\n', 1, niter, lambda, elapsedTime);
+disp(msg);
+
+end
