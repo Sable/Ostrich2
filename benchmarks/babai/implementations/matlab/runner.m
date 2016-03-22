8002b59c19c974c1a3927dc1a4194b5d621c8845
Erick Lavoie <github@ericklavoie.com>
2016-03-22 12:41:45 +0100

    babai matlab: changed output method to use disp

--- benchmarks/babai/implementations/matlab/runner.m
+++ benchmarks/babai/implementations/matlab/runner.m
@@ -4,6 +4,14 @@ function runner(size)
 	tic;
 	[vec] = babai(R, Y);
 	elapsedTime = toc;
-  svec = sprintf('%f', vec);
-	disp('{ "status": 0, "options": "runner(%s)", "time": %f,  "output": "%s" }\n', num2str(size), elapsedTime, svec);
+    disp('{');
+    disp('"options": ');
+    disp(size);
+    disp(',"time":');
+    disp(elapsedTime);
+    %disp(',"output:');
+    %disp(vec);
+    disp('}');
+    %svec = sprintf('%f', vec);
+	%disp('{ "status": 0, "options": "runner(%s)", "time": %f,  "output": "%s" }\n', num2str(size), elapsedTime, svec);
 end
